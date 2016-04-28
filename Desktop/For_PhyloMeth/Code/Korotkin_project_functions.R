32230118834e5b9e25a26ce70c789921c768d355
hkorotkin <hkorotki@vols.utk.edu>
2016-04-28 11:27:25 -0400

    Well, it runs...

--- Desktop/For_PhyloMeth/Code/Korotkin_project_functions.R
+++ Desktop/For_PhyloMeth/Code/Korotkin_project_functions.R
@@ -1,29 +1,39 @@
 #Counting changes in tip states for discrete trait in ancestral state reconstruction
 #Randomizing character traits then sampling over x amount of iterations and then counting the number of changes
 
-count <- 0
 
-trait.changes.asr <- function (tree, traits) {
-	for (i in 1:1000) {
+trait.changes.asr <- function (tree, traits, nreps=10) {
+	res.final <- c()
+	for (i in 1:nreps) {
+		print(i)	
+		tree.tmp <- tree
 		random.traits<-traits
 		random.traits[,1]<-sample(random.traits[,1],replace=FALSE)
 		#this samples the traits in the "traits" column randomly, without replacement
-		lik<-make.bisse(tree,random.traits)
-		p<-starting.point.bisse(tree)
+		char1<-random.traits[,1]
+		names(char1) <- row.names(traits)
+		lik<-make.bisse(tree.tmp,char1)
+		p<-starting.point.bisse(tree.tmp)
 		fit<-find.mle(lik,p)
 		st<-asr.marginal(lik,coef(fit))
-		round(coef(fit),5)
+		tree.tmp$node.label<-apply(t(st), 1, which.max)
+		total.states<-c(random.traits[,1],tree.tmp$node.label)
+		anc <- unique(tree.tmp$edge[,1])
+		tree.tmp <- reorder(tree.tmp, "pruningwise")
+		nb.nodes <- Nnode(tree.tmp)
+		change.count <- 0
+		for(j in seq(from=1, length.out=nb.nodes)){
+			focal <- anc[j]
+			desRows <- which(tree.tmp$edge[,1]==focal)
+			desNodes <- tree.tmp$edge[desRows,2]
+			for(desIndex in sequence(length(desRows))){
+				if(!total.states[focal] == total.states[desNodes[desIndex]]){
+					change.count = change.count+1
+				}
+			}			
+		}
+		res.final <- c(res.final, change.count)
 	}
-	
+	return(res.final)
 }
 
-
-
-
-
-#loop will be the shuffling and bisse functions
-replicate()
-?sample
-#how to count changes?
-
-#table with randomized values to use in the loop. copy original table. sample from randomized column
